ffcc8d29a595a416077dd114d58063615473a8db
Mishanua <mishasikolenko@mail.ru>
2022-10-24 11:29:43 +0300

    Include imgui

--- OwlCage/includs.hpp
+++ OwlCage/includs.hpp
@@ -1,25 +1,29 @@
-#pragma omce
-
-#define _SILENCE_STDEXT_HASH_DEPRECATION_WARNINGS 1
-#define D3D_COMPILE_STANDARD_FILE_INCLUDE ((ID3DInclude*)(UINT_PTR)1)
+#pragma once
 
 //./GUI/imgui.cpp ./GUI/backends/imgui_impl_dx11.cpp
 
-#include "../SimplestFunctions/Function.hpp"
-#include "../SimplestFunctions/Constant.hpp"
-#include "../SimplestFunctions/Power.hpp"
-#include "../SimplestFunctions/Cosine.hpp"
-#include "../SimplestFunctions/Sine.hpp"
-#include "../SimplestFunctions/Exponential.hpp"
-#include "../SimplestFunctions/Logorithmic.hpp"
-#include "../SimplestFunctions/FunctionCombine.hpp"
+#include "SimplestFunctions/Function.hpp"
+#include "SimplestFunctions/Constant.hpp"
+#include "SimplestFunctions/Power.hpp"
+#include "SimplestFunctions/Cosine.hpp"
+#include "SimplestFunctions/Sine.hpp"
+#include "SimplestFunctions/Exponential.hpp"
+#include "SimplestFunctions/Logorithmic.hpp"
+#include "SimplestFunctions/FunctionCombine.hpp"
+
+#include "imguiH/imgui.h"
+#include "imguiH/imgui_impl_glfw.h"
+#include "imguiH/imconfig.h"
+#include "imguiH/imgui_impl_opengl3.h"
+#include "imguiH/imgui_internal.h"
+#include "imguiH/imstb_rectpack.h"
+#include "imguiH/imstb_textedit.h"
+#include "imguiH/imstb_truetype.h"
 
 
-#include "../GUI/imgui.h"
-#include "../GUI/backends/imgui_impl_win32.h"
-#include "../GUI/backends/imgui_impl_dx11.h"
-#include <d3d11.h>
-#include <tchar.h>
+
+//#include <d3d11.h>
+//#include <tchar.h>
 
 
 // #include "./GUI/imgui.h"
